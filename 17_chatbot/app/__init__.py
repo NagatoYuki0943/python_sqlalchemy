b08747350a862f7205816e5c6da471bb8d098c47
NagatoYuki0943 <lihongtu12138@outlook.com>
2024-10-20 15:06:14 +0800

    更新 Noqa

--- 17_chatbot/app/__init__.py
+++ 17_chatbot/app/__init__.py
@@ -1,11 +1,11 @@
 from .core import engine, Session, Base  # Noqa
 from .models import ConversationDB, ModelDB, UserDB  # Noqa
 from .dependencies import (
-    get_password_hash,
-    verify_password,
-    random_uuid,
-    random_uuid_int,
-)  # Noqa
+    get_password_hash,  # Noqa
+    verify_password,  # Noqa
+    random_uuid,  # Noqa
+    random_uuid_int,  # Noqa
+)
 
 
 # Create all tables
