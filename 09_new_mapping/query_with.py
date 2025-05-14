6a37941c680c8992fcbcc8480c730c7ec7eb1859
NagatoYuki0943 <72508155+NagatoYuki0943@users.noreply.github.com>
2025-05-14 13:02:26 +0800

    Create query_with.py

---
+++ 09_new_mapping/query_with.py
@@ -0,0 +1,16 @@
+import time
+from db_init import Session, Person
+
+
+with Session() as session:
+    c = Person(name="Perfume", birthday="2000-10-1")
+
+    session.add(c)
+
+    session.commit()
+    session.flush()
+
+    time.sleep(5)
+
+    session.query(Person).filter_by(name="Perfume").update({"birthday": "2000-10-2"})
+    session.commit()
